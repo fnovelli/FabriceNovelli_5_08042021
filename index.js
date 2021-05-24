7860fb12985958f92bafd974a51052d9fb3b1d7f
fnovelli <f.novelli@hotmail.fr>
2021-05-24 16:42:37 +0200

    code cleanup

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 let apiUrl = 'http://localhost:3000/api/';
 
 
+function addProductInfoToCard(divType, curCard, className) {
+    curCard.appendChild(divType);
+    divType.classList.add(className);
+}
+
   //Check and display all the products available
 function listProduct(teddies) {
     let res = document.getElementById('fonctionBlock');
@@ -9,29 +14,29 @@ function listProduct(teddies) {
 
         for (let i = 0; i < teddies.length; i++) {
 
+            //create original card 
             let card = document.createElement("a");
-            res.appendChild(card);
-            card.classList.add("bCard");
-            card.href = "products.html?id=" + teddies[i]._id; //add the product ID to the URL so we can use it after
+            addProductInfoToCard(card, res, "bCard");
+            card.href = "products.html?id=" + teddies[i]._id;     //add the product ID to the URL so we can use it after
 
+            //add picture of the product
             let img = document.createElement("img");
-            card.appendChild(img);
-            img.classList.add("bImg");
+            addProductInfoToCard(img, card, "bImg");
             img.src = teddies[i].imageUrl;
         
+            //add name of the product
             let name = document.createElement("div");
-            card.appendChild(name);
-            name.classList.add("bTitle");
+            addProductInfoToCard(name, card, "bTitle");
             name.textContent = teddies[i].name;
 
+            //add description of the product
             let desc = document.createElement("div");
-            card.appendChild(desc);
-            desc.classList.add("bText");
+            addProductInfoToCard(desc, card, "bText");
             desc.textContent = teddies[i].description;
 
+            //add price of  the product
             let price = document.createElement("div");
-            card.appendChild(price);
-            price.classList.add("bPrice");     
+            addProductInfoToCard(price, card, "bText");
             price.textContent = teddies[i].price / 100 + '€'; //fix price display
         }
     }
--- products.js
+++ products.js
@@ -1,5 +1,7 @@
 let apiUrl = 'http://localhost:3000/api/';
 let stock = 30;
+let colorForm = 2;
+let qtyForm = 4;
 
 function getTeddyID() { //get the ID from the product from the URL page.
     return new URL(window.location.href).searchParams.get('id')
@@ -13,93 +15,48 @@ async function getAPI() {
        }
 }
 
+function AddColorOption(curTed, curClass) {
+  for (let i = 0; i < curTed.colors.length; i++) {
+    let colorChoice = document.createElement("option");
+    colorChoice.setAttribute("value", curTed.colors[i])
+    colorChoice.innerHTML = curTed.colors[i];
+    curClass.appendChild(colorChoice); 
+  }
+}
+
+function AddQtyOption(stock, curClass) {
+  for (let i = 1; i < stock + 1; i++) { //We want to start with 1 and not 0
+    let curQty = document.createElement("option");
+    curQty.setAttribute("value", [i]) 
+    curQty.innerHTML = [i];
+    curClass.appendChild(curQty); 
+}
+}
+
   //Display product according to its current ID.
 function displayCurrentProduct(curTed) {
     let res = document.getElementById('fonctionBlock');
 
     if (res) {
-            let card = document.createElement("div");
-            res.appendChild(card);
-            card.classList.add("bCardDetail");
 
-            let img = document.createElement("img");
-            card.appendChild(img);
-            img.classList.add("bImg");
-            img.src = curTed.imageUrl;
+            //add current product information (title, image etc.) to the card.
+            let img = document.getElementsByClassName('bImg');
+            img[0].src = curTed.imageUrl;
+        
+            let name = document.getElementsByClassName('bTitle');
+            name[0].textContent = curTed.name;
+ 
+            let getText = document.getElementsByClassName('bText');
+            getText[0].textContent = curTed.description;
 
-            
-            let name = document.createElement("div");
-            card.appendChild(name);
-            name.classList.add("bTitle");
-            name.textContent = curTed.name;
-
-            let desc = document.createElement("div");
-            card.appendChild(desc);
-            desc.classList.add("bText");
-            desc.textContent = curTed.description;
-
-
-            let price = document.createElement("div");
-            card.appendChild(price);
-            price.classList.add("bPrice");     
-            price.textContent = "Prix: " + curTed.price / 100 + " €";
+            let price = document.getElementsByClassName('bPrice');
+            price[0].textContent = curTed.price / 100 + " €";
 
             //Add an option to customize the product
-            let form = document.createElement("label");
-            let texForm = document.createElement("div");  
-            //create label for color choice
-            let color = document.createElement("select");
-            form.classList.add("bText");
+            AddColorOption(curTed, getText[colorForm]);            
+            AddQtyOption(stock, getText[qtyForm]);
 
-            card.appendChild(form);
-            form.appendChild(texForm);
-            form.appendChild(color);
-      
-
-            texForm.textContent = "Couleur: ";
-            
-            //add each color to the label
-            for (let i = 0; i < curTed.colors.length; i++) {
-                let colorChoice = document.createElement("option");
-                colorChoice.setAttribute("value", curTed.colors[i])
-                colorChoice.innerHTML = curTed.colors[i];
-                color.appendChild(colorChoice);
-            }
-
-
-            //Add an option to select the quantity
-            let form2 = document.createElement("label");
-            let texForm2 = document.createElement("div");  
-            //create label for color choice
-            let qty = document.createElement("select");
-            form2.classList.add("bText");
-
-            card.appendChild(form2);
-            form2.appendChild(texForm2);
-            form2.appendChild(qty);
-      
-
-            texForm2.textContent = "Quantité: ";
-            
-            //add the quantity to the label
-            for (let i = 1; i < stock + 1; i++) { //We want to start with 1 and not 0
-                let curQty = document.createElement("option");
-                curQty.setAttribute("value", [i]) 
-                curQty.innerHTML = [i];
-                qty.appendChild(curQty);
-            }
-
-    
-            //Add a button to add to the cart
-            let btnCart = document.createElement("a");
-            card.appendChild(btnCart);
-            btnCart.setAttribute("id", "btnAddCart");
-            let btnText = document.createElement("div");
-            btnCart.appendChild(btnText);
-            btnText.classList.add("btnText");
-            btnText.textContent = "Ajouter au Panier";
-            
-            //add the product if user click on the button
+            //add the product if user click on the add product button
             let getCart = document.getElementById("btnAddCart");
              getCart.addEventListener('click', (event) =>{
                 let cartQty = document.getElementById("cartQty");
@@ -108,8 +65,6 @@ function displayCurrentProduct(curTed) {
           });
         }
 }
-
-
 
 
 getAPI();
