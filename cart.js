9a1ddb90c2e6b522e35ad3e8a62ec046f4cbf8a1
fnovelli <f.novelli@hotmail.fr>
2021-05-26 17:06:38 +0200

    added buttons for order and clean cart

--- cart.js
+++ cart.js
@@ -1,27 +1,17 @@
-
-//Display product according to its current ID.
-function displayCurrentProduct(curTed) {
-    let res = document.getElementById('fonctionBlock');
-
-    if (res) {
-
-            //add current product information (title, image etc.) to the card.
-            let img = document.getElementsByClassName('bImg');
-            img[0].src = curTed.imageUrl;
-        
-            let name = document.getElementsByClassName('bTitle');
-            name[0].textContent = curTed.name;
- 
-            let getText = document.getElementsByClassName('bText');
-            getText[0].textContent = curTed.description;
-
-            let price = document.getElementsByClassName('bPrice');
-            price[0].textContent = curTed.price / 100 + " €";
+let res = document.getElementById('fonctionBlock');
+let card = document.getElementsByClassName('bCardDetail');
 
 
-        }
+function displayEmptyCart() {
+  if (res) {
+    let msg = document.getElementById('message');
+    if (msg)
+    {
+      msg.innerHTML = "Votre panier est vide. Commencer à ajouter des articles dès maintenant!"
+    }
+
+  }
 }
-
 
 async function loadCartProduct() {
 
@@ -33,8 +23,69 @@ async function loadCartProduct() {
         let teddies = await result.json();
          await displayCurrentProduct(teddies);
      }   
+  }
+  else {
+      await displayEmptyCart(); 
+  }
+}
+
+function addListenerCartBtn() {
+let getCart = document.getElementById("btnClearCart");
+
+if (getCart) {
+getCart.addEventListener('click', (event) =>{
+
+   localStorage.clear();
+   location.reload();
+});
+
+}
+}
+
+function createCartBtn(type, text) 
+{
+  let btnType = document.getElementById(type);
+  let div = document.createElement("div");
+ 
+  btnType.appendChild(div);
+  div.innerHTML = text;
+  div.classList.add('btnText');
+}
+
+//Display product according to its current ID.
+function displayCurrentProduct(curTed) {
+
+    if (res) {
+
+       //add current product information (title, image etc.) to the card.
+        let img = document.getElementsByClassName('bImg');
+        img[0].src = curTed.imageUrl;
+
+        //add name of the product
+        let name = document.createElement("div");
+        addProductInfoToCard(name, card[0], "bTitle");
+        name.textContent = curTed.name;
+
+        //add description of the product
+        let desc = document.createElement("div");
+        addProductInfoToCard(desc, card[0], "bText");
+        desc.textContent = curTed.description;
+
+        //add price of  the product
+        let price = document.createElement("div");
+        addProductInfoToCard(price, card[0], "bPrice");
+        price.textContent = curTed.price / 100 + '€'; //fix price display
+
+        //add button for order
+        createCartBtn("btnAddCart", "Commander");
+
+        //add button to clear cart
+        createCartBtn("btnClearCart", "Vider le Panier");
+        addListenerCartBtn();
+
     }
 }
+
 
 loadCartProduct();
 
