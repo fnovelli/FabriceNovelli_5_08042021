944f0c6d1af3e836c64bdc1232f3681c22dac065
fnovelli <f.novelli@hotmail.fr>
2021-06-03 16:54:12 +0200

    get contact info from form WIP

--- order.js
+++ order.js
@@ -1,4 +1,3 @@
-
 class orderContact {
     constructor(firstName, lastName, address, city, email) {
         this.firstName = firstName;
@@ -8,3 +7,26 @@ class orderContact {
         this.email = email;
     }
 }
+
+        //get contact info from Form.
+            let getBtnOrder = document.getElementById("btnOrder");
+
+             getBtnOrder.addEventListener('click', (event) =>{
+  
+            getFormContent = [];
+            let name = document.getElementById('firstName').value;
+            let lastName = document.getElementById('lastName').value;
+            let address = document.getElementById('address').value;
+            let city = document.getElementById('city').value;
+            let mail = document.getElementById('email').value;
+
+            let contact = new orderContact(name, lastName, address, city, mail);
+            getFormContent.push(contact);
+
+              console.log(contact);
+              
+              event.preventDefault();
+
+          });
+
+        
