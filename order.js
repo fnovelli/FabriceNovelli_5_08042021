3b32e17ea9825e5ca425877479a613530d99bba7
fnovelli <f.novelli@hotmail.fr>
2021-06-04 17:47:18 +0200

    get ID product from order

--- order.js
+++ order.js
@@ -6,27 +6,50 @@ class orderContact {
         this.city = city;
         this.email = email;
     }
+
 }
 
-        //get contact info from Form.
-            let getBtnOrder = document.getElementById("btnOrder");
 
-             getBtnOrder.addEventListener('click', (event) =>{
-  
-            getFormContent = [];
-            let name = document.getElementById('firstName').value;
-            let lastName = document.getElementById('lastName').value;
-            let address = document.getElementById('address').value;
-            let city = document.getElementById('city').value;
-            let mail = document.getElementById('email').value;
 
-            let contact = new orderContact(name, lastName, address, city, mail);
-            getFormContent.push(contact);
+async function getIDFromCart(getIDProduct) {
+    let cartCont = JSON.parse(localStorage.getItem("getCartContent"));
+    if (cartCont) {
+        for (let i = 0; i < cartCont.length; i++) {
+            let result = await getAPI(cartCont[i].id);
 
-              console.log(contact);
-              
-              event.preventDefault();
+            if (result.ok) {
+                getIDProduct.push(cartCont[i].id);
+                console.log("added ID " + cartCont[i].id);
+            }
+        }
+    }
+}
 
-          });
 
+    //get contact info from Form.
+    let getBtnOrder = document.getElementById("btnOrder");
+
+    getBtnOrder.addEventListener('click', (event) =>{
+
+    getFormContent = [];
+    getIDProduct = [];
+
+
+    let name = document.getElementById('firstName').value;
+    let lastName = document.getElementById('lastName').value;
+    let address = document.getElementById('address').value;
+    let city = document.getElementById('city').value;
+    let mail = document.getElementById('email').value;
+
+    let contact = new orderContact(name, lastName, address, city, mail);
+
+
+    getFormContent.push(contact);
+    console.log("PUSHED");
+    getIDFromCart(getIDProduct);
+    console.log(contact);
+    event.preventDefault();
+
+
+});
         
